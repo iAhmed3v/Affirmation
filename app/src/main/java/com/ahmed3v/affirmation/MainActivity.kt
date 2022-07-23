0a861a1f778067db002cb3d5aa08ff8b1cac31a1
iAhmed3v <devahmdali@gmail.com>
2022-07-23 10:28:33 +0300

    adding RecyclerView to activity_main.xml and create list_item.xml for holding the recycler view items

--- app/src/main/java/com/ahmed3v/affirmation/MainActivity.kt
+++ app/src/main/java/com/ahmed3v/affirmation/MainActivity.kt
@@ -9,9 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textView: TextView = findViewById(R.id.textView)
-
-        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
