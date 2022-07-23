34b16d0f6fa952c40e5459395110b594d665e3a5
iAhmed3v <devahmdali@gmail.com>
2022-07-23 11:19:39 +0300

    Initializing data in MainActivity.kt class

--- app/src/main/java/com/ahmed3v/affirmation/MainActivity.kt
+++ app/src/main/java/com/ahmed3v/affirmation/MainActivity.kt
@@ -3,11 +3,24 @@ package com.ahmed3v.affirmation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ahmed3v.affirmation.adapter.ItemAdapter
 import com.ahmed3v.affirmation.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
